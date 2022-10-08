03f163ff250ba130d8b475548ded7e4c64f694a2
Replit user <>
2022-10-08 01:01:23 +0000

    Ejercicio de la segunda sesion

---
+++ sesion2/ejercicio8.py
@@ -0,0 +1,17 @@
+'''
+*********** Curso de programación acelerada en Python ************
+Date: 07-10-2022
+File: sesion/ejercicio8.py
+Autor: Programador Jesus Emmanuel Martinez Torres
+Action: detecte, números positivos, negativos y cero
+'''
+numero = int(input("Escriba un número: "))
+if (numero == 0): 
+  print("El numero",numero, " es cero")
+elif (numero > 0): 
+  print("El numero",numero, "es positivo ")
+elif (numero < 0): 
+  print("El numero",numero, "es negativo")
+
+      
+    
---
+++ sesion2/ejercicio9.py
@@ -0,0 +1,12 @@
+'''
+*********** Curso de programación acelerada en Python ************
+Date: 07-10-2022
+File: sesion/ejercicio9.py
+Autor: Programador Jesus Emmanuel Martinez Torres
+Action: preguntar contraseña a usuario y la comparar con valor registrado
+'''
+contra = (input("Escriba su contraseña : "))
+password = ("contra1234")
+
+if (password != contra): 
+  print("Error la contraseña es incorrecta")
